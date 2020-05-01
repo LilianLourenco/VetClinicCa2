cf045570cd81dca38af1851199ab8819f9752de4
LilianLourenco <lylianlourenco@gmail.com>
2020-05-01 21:14:03 +0100

    cli
    cli class

---
+++ src/controller/Cli.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class Cli {
+//command line interface
+	//this class will receive the method to execute the program
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
